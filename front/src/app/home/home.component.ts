be325c5382170a27d341d7a513bbe3e42eff41d6
Kahoul <ilyes.kahoul@epitech.eu>
2019-09-18 11:57:51 +0200

    [ADD] logout on home page

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../_services';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['login']);
   }
 
 }
