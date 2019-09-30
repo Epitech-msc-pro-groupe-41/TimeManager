c7cbfcfdbf9e533d2d2b6eaba8d23f05ebed707e
Carolina-Niranjala <c.louisthayanand@gmail.com>
2019-09-30 17:03:25 +0200

    request works in travis

--- api/src/main/java/com/timemanager/api/config/SwaggerConfig.java
+++ api/src/main/java/com/timemanager/api/config/SwaggerConfig.java
@@ -1,15 +1,21 @@
 package com.timemanager.api.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,13 +35,16 @@ public class SwaggerConfig {
      * Init where to check folder for routes
      */
     public Docket api() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new ApiKey("Bearer", HttpHeaders.AUTHORIZATION, "header"));
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 //.apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.timemanager.core"))  //
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(schemeList);
     }
 
     /**
