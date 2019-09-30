52ba57a02a907dfdc8f39d5c9472d53c8d8e7dda
Kahoul <ilyes.kahoul@epitech.eu>
2019-09-30 22:47:19 +0200

    [Fix] goBack for user show

--- front/src/app/_helpers/auth.guard.ts
+++ front/src/app/_helpers/auth.guard.ts
@@ -1,37 +1,42 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { UserService } from '../_services';
-import { NotificationsService } from '../_services';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {UserService} from '../_services';
+import {NotificationsService} from '../_services';
+
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-    constructor(
-        private router: Router,
-        private userService: UserService,
-        private notifs: NotificationsService
-    ) { }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private notifs: NotificationsService
+  ) {
+  }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const currentUser = this.userService.currentUserValue;
-        if (currentUser) {
-            // authorised so return true
-            const expectedRole = route.data.allowedRoles;
-            if (expectedRole != null) {
-                if (expectedRole.includes(currentUser.type)) {
-                    return true;
-                } else {
-                    this.notifs.showError('You don\'t have the rights to access this page');
-                    this.router.navigate(['/dashboard'], { queryParams: { returnUrl: state.url } });
-                    return false
-                }
-            } else
-                return true
-        } else
-            // not logged in so redirect to login page with the return url
-            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-        return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUser = this.userService.currentUserValue;
+    if (currentUser) {
+      // authorised so return true
+      const expectedRole = route.data.allowedRoles;
+      if (expectedRole != null) {
+        if (expectedRole.includes(currentUser.type)) {
+          return true;
+        } else {
+          this.notifs.showError('You don\'t have the rights to access this page');
+          this.router.navigate(['/dashboard'], {queryParams: {returnUrl: state.url}});
+          return false;
+        }
+      } else {
+        return true;
+      }
+    } else
+    // not logged in so redirect to login page with the return url
+    {
+      this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
     }
+    return false;
+  }
 }
